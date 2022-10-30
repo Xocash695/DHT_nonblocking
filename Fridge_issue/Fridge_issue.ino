8944c58dd814345ead4995e4b1f29c85665fb54b
Xocash695 <akash.kallumkal@gmail.com>
2022-10-29 20:29:59 -0400

    fixed issues with keys not working

--- Fridge_issue/Fridge_issue.ino
+++ Fridge_issue/Fridge_issue.ino
@@ -11,14 +11,13 @@ static const int DHT_SENSOR_PIN = 2;
 DHT_nonblocking dht_sensor( DHT_SENSOR_PIN, DHT_SENSOR_TYPE );
 
 int count = 0; 
-float setlow = -10.00;
+float setlow = 5.00;
 float sethigh = 10.00;
 bool Defrost = false;
 bool cycle = false;
 int incomingByte = 0; 
 bool manual = false;
-int option;
-bool safety = true;
+int option = 0;
 
 /*
  * Initialize the serial port.
@@ -64,7 +63,7 @@ void loop( )
   float temperature;
   float humidity;
   pinMode(12, OUTPUT);
-
+  
   /* Measure temperature and humidity.  If the functions returns
      true, then a measurement is available. */
   if( measure_environment( &temperature, &humidity ) == true )
@@ -92,17 +91,8 @@ void loop( )
     }
     if (manual == true) {
       Serial.println("MANUAL: ON ");
-      if (safety == true) {
-        delay(30000);
-        manual = false;
-      }
     } else {
       Serial.println("MANUAL: OFF");
-    }
-    if (safety == true) {
-      Serial.println("SAFETY: ON ");
-    } else {
-      Serial.println("SAFETY: OFF");
     }
     Serial.print( "TEMPERATURE = " );
     Serial.print( temperature, 1 );
@@ -111,65 +101,42 @@ void loop( )
     Serial.println( "%" ); 
     Serial.print("CYCLE COUNT:");
     Serial.println(count); 
-    if (Serial.available() > 0) {
+    Serial.print("ENTER 2 TO TURN ON DEFROST MANUALLY, ");
+    Serial.print("ENTER 3 TO TURN OFF DEFROST MANUALLY, ");
+    Serial.print("ENTER 4 TO NORMAL DEFROST, ");
+    Serial.println("ENTER 5 TO RESET ALL SETTINGS TO DEFAULT");
+    if (Serial.available() >= 1) {
     // read the incoming byte:
-    incomingByte = Serial.read();
+    incomingByte = Serial.parseInt();
     Serial.print("OPTION ENTERED: ");
     Serial.println(incomingByte, DEC);
-    if (incomingByte ==  1) {
-       Serial.println("ENTER SETLOW VALUE");
-       setlow = Serial.read();
-       Serial.println("ENTER SETHIGH VALUE");
-       sethigh = Serial.read();
-       Serial.println("NOTE ALL SETLOW AND SETHIGH WILL RETURN TO DEFAULT VALUES UPON RESTART!");
-
-    }
     if (incomingByte ==  2) {
-       Serial.println("MANUAL DEFROST TURN ON AND OFF");
-       Serial.println("ENTER 1 TO TURN ON DEFROST, ENTER 2 TO TURN OFF DEFROST, ENTER 3 FOR AUTO");
-       option = Serial.read();
-       switch (option) {
-        case 1: 
-          Serial.println("MANUAL DREFROST TURNED ON "); 
-          manual = true;
-          Defrost = true;
-          break;
-        case 2: 
-          Serial.println("MANUAL DREFROST TURNED OFF "); 
-          manual = true;
-          Defrost = false;
-        case 3: 
-          Serial.println("AUTO TURNED ON");
-          manual = false;
-       }
-         
+      Serial.println("MANUAL DREFROST TURNED ON "); 
+      manual = true;
+      Defrost = true;
     }
-    if (incomingByte == 3) {
-       Serial.println("SAFETY FOR MANUAL: ENTER 1 TO TURN ON, ENTER 2 TO TURN OFF");
-       option = Serial.read();
-       if (option == 1) {
-          Serial.println("SAFETY HAS BEEN TURNED ON");
-          safety = true;
-       } else if (option == 2) {
-          Serial.println("SAFETY HAS BEEN TURNED OFF");
-          Serial.println("THIS MODE IS NOT RECOMMENDED!");
-          safety = false;
-       }
-         
+    if (incomingByte ==  3) {
+         Serial.println("MANUAL DREFROST TURNED OFF "); 
+         manual = true;
+         Defrost = false;
+    }  
+    if (incomingByte == 4) {
+        Serial.println("NORMAL MODE");
+        manual = false;
     
     }
-      if (incomingByte == 4) {
+    if (incomingByte == 5) {
        Serial.println("RESETTING!");
-       safety = true;
        count = 0; 
-       setlow = -10.00;
-       sethigh = 10.00;
        Defrost = false;
        manual = false;
        Serial.println("FINISHED RESET!");
-      }
-         
+     }
+      
     
+    }
+    if (count > 2147483647) {
+      count = 0;
     }
 
     
